a5813c770e5de206ba1643cb27c260853d5897b2
srivabhi22 <srivabhi22@iitk.ac.in>
2024-07-06 16:14:07 +0530

    app created

---
+++ app.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from flask import Flask
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+if __name__=='__main__':
+    app.run()
